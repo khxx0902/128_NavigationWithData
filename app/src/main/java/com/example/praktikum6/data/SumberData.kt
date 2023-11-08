bf653fc9925582016f83b4dab8949af4421ebb88
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-11-09 01:51:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum6/data/SumberData.kt
@@ -0,0 +1,14 @@
+package com.example.praktikum6.data
+
+import com.example.praktikum6.R
+
+
+object SumberData {
+    val flavors = listOf(
+            R.string.Natural,
+            R.string.Vanila,
+            R.string.lemon,
+            R.string.milk,
+            R.string.lyche
+        )
+}
