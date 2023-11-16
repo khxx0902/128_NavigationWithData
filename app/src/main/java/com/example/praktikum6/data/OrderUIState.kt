53e06f6e84d8c25cc094cf749692978fed5c6d97
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-11-16 11:53:50 +0700

    add data class order ui state

--- app/src/main/java/com/example/praktikum6/data/OrderUIState.kt
+++ app/src/main/java/com/example/praktikum6/data/OrderUIState.kt
@@ -1,7 +1,10 @@
 package com.example.praktikum6.data
 
 data class OrderUIState(
+    var nama: String="",
+    var alamat: String="",
+    var tlp: String="",
     val jumlah: Int =0,
     val rasa: String = "",
-    val harga: String = "",
+    val harga: String = ""
 )
