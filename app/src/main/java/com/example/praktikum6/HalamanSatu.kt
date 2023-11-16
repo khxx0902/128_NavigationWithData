4955f68605afc8fd19178ddcf2e64ec12fcd4ff5
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-11-16 12:18:22 +0700

    add OutlinedTextField

--- app/src/main/java/com/example/praktikum6/HalamanSatu.kt
+++ app/src/main/java/com/example/praktikum6/HalamanSatu.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,6 +40,18 @@ fun HalamanSatu(
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ) {
+
+        OutlinedTextField(
+            value = namaTxt,
+            onValueChange = {namaTxt = it},
+            label = { Text(text = stringResource(id = R.string.nama)) }
+        )
+
+        OutlinedTextField(
+            value = namaTxt,
+            onValueChange = {namaTxt = it},
+            label = { Text(text = stringResource(id = R.string.nama)) }
+        )
 
     }
 }
