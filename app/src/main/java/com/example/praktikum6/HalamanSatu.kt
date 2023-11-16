8e24d7deebe2d711a45975933f8ba584fcc76f4d
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-11-16 12:20:05 +0700

    add Spacer

--- app/src/main/java/com/example/praktikum6/HalamanSatu.kt
+++ app/src/main/java/com/example/praktikum6/HalamanSatu.kt
@@ -3,7 +3,9 @@ package com.example.praktikum6
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,6 +55,8 @@ fun HalamanSatu(
             onValueChange = {namaTxt = it},
             label = { Text(text = stringResource(id = R.string.nama)) }
         )
+
+        Spacer(modifier = Modifier.height(15.dp))
 
     }
 }
